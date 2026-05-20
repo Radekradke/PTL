81dcafbd8661e19c580f427f1fc1128b9e4fbfe9
Andre do Nascimento Gomes <anascimento@local.lifting.com.br>
2026-05-20 14:28:12 -0300

    resolve merge conflicts

--- frontend/src/pages/auth/Login.tsx
+++ frontend/src/pages/auth/Login.tsx
@@ -151,6 +151,7 @@ export function Login() {
       </div>
 
      <Button
+
   onClick={handleLogin}
   className="mt-3 h-[52px] w-full rounded-full bg-gradient-to-r from-[#073B2A] via-[#00A859] to-[#073B2A] font-bold !text-white shadow-[0_18px_45px_rgba(0,168,89,0.28)] transition hover:-translate-y-0.5 hover:brightness-110 active:translate-y-0"
 >
